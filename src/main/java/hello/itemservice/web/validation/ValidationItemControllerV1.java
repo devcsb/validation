01b59500ae0ed2e97cdcf2395d93ae9c7fcaea38
devcsb <devcsb119@gmail.com>
2023-02-01 15:48:48 +0900

    검증 직접 처리 - 개발
    검증 로직을 직접 구현하여, 검증 상황에 맞게 에러 메세지를 작성하여 model Attribute에 삽입하여 반환하고, view 페이지에서 보여주는 방식으로 사용자 입력 검증 처리 구현

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
@@ -3,13 +3,18 @@ package hello.itemservice.web.validation;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+@Slf4j
 @Controller
 @RequestMapping("/validation/v1/items")
 @RequiredArgsConstructor
@@ -33,15 +38,45 @@ public class ValidationItemControllerV1 {
 
     @GetMapping("/add")
     public String addForm(Model model) {
+        // 입력 폼을 렌더링 할 때, 앞으로 사용될 빈 Model 객체를 만들어 넘기면,
+        // validation 에 실패했을 때, 그대로 재사용 할 수 있는 장점이 있으므로, 이런 형식으로 개발을 한다.
         model.addAttribute("item", new Item());
         return "validation/v1/addForm";
     }
 
     @PostMapping("/add")
-    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes, Model model) {
 
+        //검증 오류 결과를 보관할 Map 객체 생성
+        Map<String, String> errors = new HashMap<>();
 
+        //검증 로직
+        if (!StringUtils.hasText(item.getItemName())) {
+            errors.put("itemName", "상품 이름은 필수입니다.");
+        }
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            errors.put("price", "가격은 1,000 ~ 1,000,000 까지 허용합니다.");
+        }
+        if (item.getQuantity() == null || item.getQuantity() >= 9999) {
+            errors.put("quantity", "수량은 최대 9,999 까지 허용합니다.");
+        }
 
+        // 특정 필드가 아닌 복합 룰 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                errors.put("globalError", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice);
+            }
+        }
+
+        //검증에 실패하면 다시 입력 폼으로
+        if (!errors.isEmpty()) {  // "에러가 없는 것이 아니면" => 부정의 부정이므로, 코드 가독성이 매우 떨어짐. 실무에선 반드시 다른 방식으로 풀어서 쓰도록 한다.
+            model.addAttribute("errors", errors);
+            log.info("errors = {}", errors);
+            return "validation/v1/addForm";
+        }
+
+        //성공 로직
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
@@ -60,7 +95,6 @@ public class ValidationItemControllerV1 {
         itemRepository.update(itemId, item);
         return "redirect:/validation/v1/items/{itemId}";
     }
-
 
 
 }
