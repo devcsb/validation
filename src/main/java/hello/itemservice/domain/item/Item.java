3de50291db709dcecb3581448f90304d2b75208d
devcsb <devcsb119@gmail.com>
2023-02-12 14:22:37 +0900

    Bean Validation - 시작
    Bean Validation에 대한 이해,
    javax.validation(표준스펙)과
    hibernate.validator(구현체)의 차이 이해,
    테스트 방법 숙지

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,12 +2,28 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 
+//하이버네이트 validator 구현체를 사용할 때만 제공되는 검증 기능.(org.hibernate.validator...)
+import org.hibernate.validator.constraints.Range;
+
+// 특정 구현에 관계없이 제공되는 표준 인터페이스(javax.validation...)
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Data
 public class Item {
 
     private Long id;
+
+    @NotBlank(message = "공백은 허용하지 않습니다!") // 이렇게 메세지 정의할 수도 있고, properties 파일 바깥으로 빼낼 수도 있다.
     private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 1000000)  // 하이버네이트 validator 구현체를 사용할 때만 제공되는 검증기능.
     private Integer price;
+
+    @NotNull
+    @Max(9999)
     private Integer quantity;
 
     public Item() {
