475182dcf6e0bcabec7ed4b5b31cebfcb888ba70
devcsb <devcsb119@gmail.com>
2023-02-12 16:35:23 +0900

    Bean Validation - 오브젝트 오류
    특정 필드가 아닌, 여러 필드나 객체에 대한 오류 처리 방법 숙지
    
    1. 클래스에 @ScriptAssert 사용하여 처리(실무에서의 복잡한 검증 조건 처리하기 힘듦. 권장하지 않음.)
    2. 자바 코드로 그냥 검증 로직 작성

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -4,13 +4,16 @@ import lombok.Data;
 
 //하이버네이트 validator 구현체를 사용할 때만 제공되는 검증 기능.(org.hibernate.validator...)
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 // 특정 구현에 관계없이 제공되는 표준 인터페이스(javax.validation...)
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+// 실무에서의 복잡한 검증 조건을 @ScriptAssert 로 검증하기엔 기능이 너무 부족하다. 오브젝트 검증의 경우는 직접 자바 코드로 작성하는 편이 낫다.
 @Data
+//@ScriptAssert(lang="javascript", script = "_this.price * _this.quantity >= 10000", message = "최소 주문금액은 10000원 이상입니다.")
 public class Item {
 
     private Long id;
